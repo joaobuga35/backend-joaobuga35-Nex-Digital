453e627e69c269ca5585fa6f0869149b501f613f
joaobuga35 <joaobuga35@gmail.com>
2023-07-27 16:11:24 -0300

    feat: insert class app error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
+import "express-async-errors";
 import express from "express";
 import userRoutes from "./routers/users.routes";
+import { handleError } from "./errors";
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
+
+app.use(handleError);
 
 app.listen(3000, () => console.log("server is running"));
